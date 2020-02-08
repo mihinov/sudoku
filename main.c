f8494b94b739028d8c2d07154f0fe63f92ba5dc3
Kamahin Mihail <kamahinmihail@gmail.com>
2020-02-08 20:48:19 +0300

    2

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
 #include <unistd.h>
 
 void    ft_putchar(char letter)
 {
     write(1, &letter, 1);
+}
+
+void    ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
 }
 
 int     length_itr(int n)
@@ -81,6 +92,29 @@ void    show(int *x)
     }
 }
 
+void     calc_used(int *x, int *row, int *col, int *used)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (i < 9)
+        *used |= 1 << (x[i++ * 9 + *col] - 1);
+    while (j < 9)
+        *used |= 1 << (x[*row * 9 + j++] - 1);
+    *row = *row / 3 * 3;
+    *col = *col / 3 * 3;
+    i = *row;
+    while (i < *row + 3)
+    {
+        j = *col;
+        while (j < *col + 3)
+            *used |= 1 << (x[i * 9 + j++] - 1);
+        i++;
+    }
+}
+
 int     trycell(int *x, int pos)
 {
     int row;
@@ -96,40 +130,10 @@ int     trycell(int *x, int pos)
     used = 0;
 
     if (pos == 81)
-    {
         return (1);
-    }
     if (x[pos])
-    {
         return (trycell(x, pos + 1));
-    }
-
-
-    while (i < 9)
-    {
-        used |= 1 << (x[i * 9 + col] - 1);
-        i++;
-    }
-
-    while (j < 9)
-    {
-        used |= 1 << (x[row * 9 + j] - 1);
-        j++;
-    }
-
-    row = row / 3 * 3;
-    col = col / 3 * 3;
-    i = row;
-    while (i < row + 3)
-    {
-        j = col;
-        while (j < col + 3)
-        {
-            used |= 1 << (x[i * 9 + j] - 1);
-            j++;
-        }
-        i++;
-    }
+    calc_used(x, &row, &col, &used);
 
     x[pos] = 1;
     while (x[pos] <= 9)
@@ -138,9 +142,8 @@ int     trycell(int *x, int pos)
         used >>= 1;
         x[pos]++;
     }
-
     x[pos] = 0;
-    return 0;
+    return (0);
 }
  
 void    solve(const char *s)
@@ -148,41 +151,31 @@ void    solve(const char *s)
     int i;
     int x[81];
 
+    i = 0;
     while (i < 81)
     {
-        // x[i] = s[i] >= '1' && s[i] <= '9' ? s[i] - '0' : 0;
         if (s[i] >= '1' && s[i] <= '9')
-        {
             x[i] = s[i] - '0';
-        }
+        else if (s[i] == '.')
+            x[i] = 0;
         else
         {
-            x[i] = 0;
+            ft_putstr("no solution");
+            return ;
         }
         i++;
     }
 
     if (trycell(x, 0))
-    {
         show(x);
-    }
     else
-    {
-        puts("no solution");
-    }
+        ft_putstr("no solution");   
 }
  
 int     main(void)
 {
-  solve(  "........."
-        "........."
-        ".98....6."
-        "8...6...3"
-        "4..8.3..1"
-        "7...2...6"
-        ".6....28."
-        "...419..5"
-        "....8..79" );
+  solve(  "9...7...." "2...9..53" ".6..124.." "84...1.9." "5.....8.." ".31..4..." "..37..68."
+".9..5.741" "47......." );
  
-  return 0;
+  return (0);
 }
